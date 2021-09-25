dbbf66c3b6797ac991341752daf15129f54a6f40
p-reetu <manchekar.preeti@gmail.com>
2021-09-25 19:57:49 +0530

    mult.py added

---
+++ mult.py
@@ -0,0 +1,10 @@
+def mult(a,b,c):
+    return a*b*c
+
+print("----Multiplication of three numbers----")
+x = int(input("enter the first number: "))
+y = int(input("enter the first number: "))
+z = int(input("enter the first number: "))
+
+print("The multiplication of three numbers is")
+print(sum(x,y,z))
