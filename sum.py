36a8b7b3f9fe612f9b8471c1080f230dc93b4e72
p-reetu <manchekar.preeti@gmail.com>
2021-09-25 19:46:34 +0530

    sum.py added

---
+++ sum.py
@@ -0,0 +1,10 @@
+def sum(a,b,c):
+    return a+b+c
+
+print("----Sum of three numbers----")
+x = int(input("enter the first number: "))
+y = int(input("enter the first number: "))
+z = int(input("enter the first number: "))
+
+print("The sum of three numbers is")
+print(sum(x,y,z))
